67fdb461596d189ebf9f686a2a5d6972f133d6e0
Pawan Kumar <39653409+dheeth@users.noreply.github.com>
2022-11-01 02:29:32 +0530

    Update chart-sync.sh

--- chart-sync.sh
+++ chart-sync.sh
@@ -1,17 +1,25 @@
-git clone $INPUT_SOURCE_REPO
-cd ..
-git clone $INPUT_TARGET_REPO
-cd $INPUT_WORKING_DIR
-git remote add target https://${INPUT_GIT_TARGET_USERNAME}:${INPUT_GIT_TARGET_TOKEN}@${INPUT_TARGET_REPO#https://}
+RELEASE_FILE_CONTENTS=$(curl -L -s  "https://raw.githubusercontent.com/$REPO/$GIT_BRANCH/$RELEASE_FILE" )
+RELEASE_TYPE=$(echo $RELEASE_FILE_CONTENTS | awk '{print $1}')
+if [[ "$RELEASE_TYPE" == "stable" ]]
+  then
+    git clone $INPUT_SOURCE_REPO
+    cd ..
+    git clone $INPUT_TARGET_REPO
+    cd $INPUT_WORKING_DIR
+    git remote add target https://${INPUT_GIT_TARGET_USERNAME}:${INPUT_GIT_TARGET_TOKEN}@${INPUT_TARGET_REPO#https://}
 
-git checkout main
-git remote -v
+    git checkout main
+    git remote -v
 
-git config --global user.email ${INPUT_GIT_TARGET_USEREMAIL}
-git config --global user.name ${INPUT_GIT_TARGET_USERNAME}
+    git config --global user.email ${INPUT_GIT_TARGET_USEREMAIL}
+    git config --global user.name ${INPUT_GIT_TARGET_USERNAME}
 
-cp -r ../${INPUT_SOURCE_DIR} $INPUT_TARGET_DIR
-git add .
-git commit -m "Synching Repo"
+    cp -r ../${INPUT_SOURCE_DIR} $INPUT_TARGET_DIR
+    git add .
+    git commit -m "Synching Repo"
 
-git push target main
+    git push target main
+elif [[ "$RELEASE_TYPE" == "beta" ]]
+  then
+    echo "Not syncing charts repo due to beta release"
+fi
